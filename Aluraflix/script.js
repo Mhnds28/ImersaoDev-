ae20d3dfef5c75e703e698908e90d798e6015a67
Marcos Henrique <marcos2890@outlook.com>
2021-05-11 17:35:08 -0300

    Atualizar

---
+++ Aluraflix/script.js
@@ -0,0 +1,14 @@
+// var filmes = ["Homem aranha","Liga da justuça","Deadpool"];
+
+// var tentativas = 3
+
+// for (var i = 0; i < filmes.length; i++){
+//     console.log(filmes[i]);
+
+// }
+
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BNTMxOGI4OGMtMTgwMy00NmFjLWIyOTUtYjQ0OGQ4Mjk0YjNjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYjI3NDg0ZTEtMDEwYS00YWMyLThjYjktMTNlM2NmYjc1OGRiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BNjk1Njk3YjctMmMyYS00Y2I4LThhMzktN2U0MTMyZTFlYWQ5XkEyXkFqcGdeQXVyODM2ODEzMDA@._V1_UY268_CR43,0,182,268_AL_.jpg"];
+
+for (var i = 0; i < listaFilmes.length; i++){
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
